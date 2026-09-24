25bb20855ebb077f56a4679be8104cb7895b272d
techitechi0331-svg <techitechi0331@gmail.com>
2026-09-25 04:47:29 +0900

    name default VST3 program in Density

--- src/plugins/Density/PluginProcessor.h
+++ src/plugins/Density/PluginProcessor.h
@@ -22,7 +22,7 @@ public:
     int getNumPrograms() override { return 1; }
     int getCurrentProgram() override { return 0; }
     void setCurrentProgram (int) override {}
-    const juce::String getProgramName (int) override { return {}; }
+    const juce::String getProgramName (int) override { return "Default"; }
     void changeProgramName (int, const juce::String&) override {}
     void getStateInformation (juce::MemoryBlock&) override;
     void setStateInformation (const void*, int) override;

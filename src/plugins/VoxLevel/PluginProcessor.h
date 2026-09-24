b50dc1ef0db845af81a7bb637f1cc50f37a4a7ec
techitechi0331-svg <techitechi0331@gmail.com>
2026-09-25 04:47:32 +0900

    name default VST3 program in VoxLevel

--- src/plugins/VoxLevel/PluginProcessor.h
+++ src/plugins/VoxLevel/PluginProcessor.h
@@ -25,7 +25,7 @@ public:
     int getNumPrograms() override { return 1; }
     int getCurrentProgram() override { return 0; }
     void setCurrentProgram (int) override {}
-    const juce::String getProgramName (int) override { return {}; }
+    const juce::String getProgramName (int) override { return "Default"; }
     void changeProgramName (int, const juce::String&) override {}
 
     void getStateInformation (juce::MemoryBlock&) override;

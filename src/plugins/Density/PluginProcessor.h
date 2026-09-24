2de9c862849dfa7113064ce7890f646e212a1b57
techitechi0331-svg <techitechi0331@gmail.com>
2026-09-25 03:02:44 +0900

    add Density processor header

---
+++ src/plugins/Density/PluginProcessor.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <JuceHeader.h>
+
+class DensityAudioProcessor final : public juce::AudioProcessor
+{
+public:
+    DensityAudioProcessor();
+    ~DensityAudioProcessor() override = default;
+
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override {}
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override { return true; }
+    const juce::String getName() const override { return JucePlugin_Name; }
+    bool acceptsMidi() const override { return false; }
+    bool producesMidi() const override { return false; }
+    bool isMidiEffect() const override { return false; }
+    double getTailLengthSeconds() const override { return 0.0; }
+    int getNumPrograms() override { return 1; }
+    int getCurrentProgram() override { return 0; }
+    void setCurrentProgram (int) override {}
+    const juce::String getProgramName (int) override { return {}; }
+    void changeProgramName (int, const juce::String&) override {}
+    void getStateInformation (juce::MemoryBlock&) override;
+    void setStateInformation (const void*, int) override;
+
+private:
+    static juce::AudioProcessorValueTreeState::ParameterLayout createLayout();
+
+    juce::AudioProcessorValueTreeState apvts;
+    std::unique_ptr<juce::dsp::Oversampling<float>> oversampling;
+    juce::AudioBuffer<float> dryBuffer;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DensityAudioProcessor)
+};

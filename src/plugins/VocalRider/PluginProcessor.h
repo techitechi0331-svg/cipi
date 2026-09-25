ddca5bd65d70204f2e80be777cfa678b59c41c00
techitechi0331-svg <techitechi0331@gmail.com>
2026-09-25 09:33:00 +0900

    testability: make Vocal Rider processor name explicit

--- src/plugins/VocalRider/PluginProcessor.h
+++ src/plugins/VocalRider/PluginProcessor.h
@@ -17,7 +17,7 @@ public:
 
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override { return true; }
-    const juce::String getName() const override { return JucePlugin_Name; }
+    const juce::String getName() const override { return "CIPI Vocal Rider"; }
     bool acceptsMidi() const override { return false; }
     bool producesMidi() const override { return false; }
     bool isMidiEffect() const override { return false; }
